ffba49c67dff2a1442bba2de5b86105edc5a0e7b
ReginaRen1124 <125526026+ReginaRen1124@users.noreply.github.com>
2023-09-17 16:17:17 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function addUser{
+j1 = document.getElementById("j1").value;
+j2 = document.getElementById("j2").value;
+
+localStorage.setItem("j1", j1);
+localStorage.setItem("j2", j2);
+
+window.location = "quiz_game_page.html";
+}
